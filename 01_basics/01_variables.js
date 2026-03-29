ece6f38efbfd8d0b161c67bd053d764669ce0f23
sakshigondhane007 <sakshigondhane007@gmail.com>
2026-03-29 19:11:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "sakshigondhane07@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 const variable cannot changed/update
+
+accountEmail = "sakshi30@gmail.com"
+accountPassword = "121212"
+accountCity = "Ahmednagar"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+/*
+Prefer not to use var
+because of issues in block scope and functional scope
+*/
