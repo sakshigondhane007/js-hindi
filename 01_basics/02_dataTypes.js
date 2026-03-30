20c28e68ff42813b6a272bec09b75007e7ff5f3d
sakshigondhane007 <sakshigondhane007@gmail.com>
2026-03-30 05:34:47 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js codeas a newer version
+
+// alert( 3 + 3) //we are using node.js, not browser
+
+console.log(3
+    +
+     3);  //code readability should be high
+     
+console.log("Sakshi")
+
+let name = "Sakshi"
+let age = 18
+let isLoggedIn = false
+let state;  
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assign
+// symbol => unique 
+// this are primitive data types
+
+// object
+
+console.log(typeof "sakshi");
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
